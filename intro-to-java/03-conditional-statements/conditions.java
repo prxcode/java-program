5c7d06557aeb530c794c36f8cfc02f5dcb442fb2
Priyanshu <183171126+prxcode@users.noreply.github.com>
2025-07-05 13:02:57 +0400

    Update conditions.java

--- intro-to-java/03-conditional-statements/conditions.java
+++ intro-to-java/03-conditional-statements/conditions.java
@@ -1,5 +1,6 @@
-import java.util.Scanner;
-
+import java.util.Scanner; //it is required to import for input user data like in python input is built-in function, age = int(input("Enter age: "))
+// also note class name which is "conditions" here should always be same as <filename.java> 
+// rest is similar to C lang
 public class conditions {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in); // Scanner is used to take input from the user
