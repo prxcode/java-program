f71dfc2e60bdfcd4586bf29fab8120f546bf697b
Priyanshu <183171126+prxcode@users.noreply.github.com>
2025-07-19 23:46:43 +0300

    Add files via upload

---
+++ intro-to-java/03-operators-and-expression/operators.java
@@ -0,0 +1,21 @@
+
+
+// Print stars in a pattern
+
+public class operators {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 6 + a;
+        int c = 6 % a;
+        int d = 5;
+        d += 5;
+        System.out.println(b); // b= 6 + 4 = 10
+        System.out.println(c); // c= 6 % 4 = 2
+        System.out.println(d); 
+        System.out.println(a>d);
+        System.out.println(a>d && b>a); // false AND true = false
+        System.out.println(a>d || b>a); // true OR true = true
+        System.out.println(2&3);
+    }
+}
+
