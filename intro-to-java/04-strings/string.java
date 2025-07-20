d20138242ef861e497371896e91886398e022551
Priyanshu <183171126+prxcode@users.noreply.github.com>
2025-07-20 17:40:42 +0300

    Add files via upload

---
+++ intro-to-java/04-strings/string.java
@@ -0,0 +1,19 @@
+
+public class string {
+    public static void main(String[] args) {
+        String a = "Hello";
+        String b = "World";
+        int value = b.length();
+        System.out.println(a);
+        System.out.println(value);
+
+
+        String name = "   Priyanshu";
+        String lower = name.toLowerCase(); // write name.tol for faster typing in IDE
+        String upper = name.toUpperCase(); // write name.tuc for faster typing in IDE
+        System.out.println(lower);
+        System.out.println(upper);
+        System.out.println(name.toLowerCase());
+        System.out.println(name.trim());
+    }    
+}
