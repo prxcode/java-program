d9c01876a9e28f1781aabff929baa5c58a2e8dde
Priyanshu <183171126+prxcode@users.noreply.github.com>
2025-07-31 12:20:36 +0300

    Update multidimensional.java

--- intro-to-java/07-array/multidimensional.java
+++ intro-to-java/07-array/multidimensional.java
@@ -1,5 +1,27 @@
-public class multidimensional{
-  Public static void Main(String[], args){
-  int[] = marks
-  }
+
+public class multidimarray {
+    public static void main(String[] args) {
+        int[] marks; // 1D Array
+        int[][] flats; // 2D Array
+        flats = new int[2][3]; // 2D Array with 2 rows and 3 columns
+        flats[0][0] = 101;
+        flats[0][1] = 102;
+        flats[0][2] = 103;
+        flats[1][0] = 201;
+        flats[1][1] = 202;
+        flats[1][2] = 203;
+
+        // Displaying 2D Array using for loop
+        System.out.println("Printing 2D Array using for loop");
+        for(int i=0;i<flats.length;i++) {
+            for(int j=0;j<flats[i].length;j++) {
+                System.out.print(flats[i][j]);
+                System.out.print(" ");
+            }
+            System.out.print("\n");
+
+    }
+
+        
+    }
 }
