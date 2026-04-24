3688eabfabe6b1f74ec27204c0cfaecb0a10abba
Priyanshu <183171126+prxcode@users.noreply.github.com>
2026-04-24 16:57:15 +0400

    Update statement.java

--- intro-to-java/05-conditionals/statement.java
+++ intro-to-java/05-conditionals/statement.java
@@ -12,5 +12,28 @@ public class statement {
        }
     }
 }
+
+// or you can also write like this 
+
+import java.util.*;
+
+public class Main{
+    public static void main(String args[]){
+        System.out.println("Hello long time no see?");
+        System.out.print("Enter your age: ");
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
         
-    
+        if (age>0 && age<18){
+            System.out.println("you are kid!");
+        }
+        else if(age <200 && age >= 18){
+            System.out.println("your are older!");
+        }
+        else {
+            System.out.println("Enter valid age!!");
+        }
+        
+        
+    }
+}
