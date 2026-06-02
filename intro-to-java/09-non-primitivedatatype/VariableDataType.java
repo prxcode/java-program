aa20c4861b9655c8f45c89a931b4b7cf98c12bc1
Priyanshu <183171126+prxcode@users.noreply.github.com>
2026-06-02 23:02:39 +0400

    Create VariableDataType class for Student example
    Added a VariableDataType class to demonstrate usage of a Student object with attributes.

---
+++ intro-to-java/09-non-primitivedatatype/VariableDataType.java
@@ -0,0 +1,18 @@
+public class VariableDataType{
+  public static void main(String args[]){
+    Student s1 = new Student();
+    s1.name = "Priyanshu";
+    s1.phone = "123456";
+    s1.rollNumber = "0001";
+    s1.city = "Delhi";
+
+  Student s2 = new Student();
+    s1.name = "Elliot";
+    s1.phone = "9999999";
+    s1.rollNumber = "0010";
+    s1.city = "New York";
+
+  System.out.println("1st student name is "+ s1.name);
+  System.out.println("1st student roll number is "+ s1.rollNumber);
+  }
+}
